056acd7ad03b5faf37e6fb237f5e36a9d998e5e7
Nexerum <timhoglund@DAT-1584.local>
2017-01-23 13:46:59 +0200

    Uppdateratrad till upp 5

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 
-<?php
+<!-- <?php
  	if ($_GET ['name'] == NULL):
 ?>
 
@@ -14,9 +14,9 @@
 	<label for="name">Name</label>
 	<input type="text" name="name" id="name">
 	<input type="submit" value="Send!">
-</form>
+</form> -->
 
-<?php
+<!-- <?php
 
 else:
 
@@ -27,8 +27,8 @@ $name_end = mb_substr($name, $half_name_lenght, $remaining_chars);
 $name_beginning = mb_substr($name, 0, $half_name_lenght);
 $botname = $name_end . $name_beginning;
 
-?>
-
+?> -->
+<!-- 
 <h1><?= $botname ?></h1>
 
 <?php 
@@ -37,13 +37,13 @@ if ($_GET ['search'] == NULL):
 ?>
 
 <form>
-	<label for="search">fråga?</label>
+	<label for="search">Fråga</label>
 	<input type="text" name="search" id="search">
 	<input type="submit" value="Fråga!">
 	<input type="hidden" name="name" value="<?= $name ?>">
-</form>
+</form> -->
 
-
+<!-- 
 <?php  
 else:
 $lander = [
@@ -61,8 +61,56 @@ for ($i = 0; $i < count($lander); $i++) {
 }
 
 ?>
+ 
+<?php endif ?>
+
+-->
+<?php
+if ($_GET['name'] == NULL):
+?>
+
+
+<form>
+	<label for="name">Name</label>
+	<input type="text" name="name" id="name">
+	<input type="submit" value="Send!">
+</form>
+
+<?php
+elseif ($_GET['reply'] == NULL): 
+	$name = $_GET['name'];
+	$half_name_length = (int) (mb_strlen($name) / 2);
+	$remaining_chars = mb_strlen($name) - $half_name_length;
+	$name_end = mb_substr($name, $half_name_length, $remaining_chars);
+	$name_beginning = mb_substr($name, 0, $half_name_length);
+	$botname = $name_end . $name_beginning;
+?>
+
+<p><strong><?= $botname ?>:</strong> Hej <?= $name ?></p>
+<form>
+	<input type="text" name="reply">
+	<input type="hidden" name="name" value="<?= $name ?>">
+	<input type="hidden" name="botname" value="<?= $botname ?>">
+	<input type="submit" value="reply">
+</form>
+<?php
+else:
+	$name = $_GET['name'];
+	$botname = $_GET['botname'];
+	$reply = $_GET['reply'];
+?>
+<p><strong><?= $botname ?>:</strong> Hej <?= $name ?></p>
+<p><strong><?= $name ?>:</strong> <?= $reply ?></p>
+
+<?php
+$array = ["Fint väder?", "Hur mår du"];
+$botreply = $array[random_int(0, count($array) - 1)];
+?>
+
+<p><strong><?= $botname ?>:</strong> <?= $botreply ?></p>
 
 <?php endif ?>
+
 
 </body>
 </html>
